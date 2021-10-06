e6d136c2ca9e7904b368be8b25606d911b9a5728
Josefine Holth <Josefine.Holth@hotmail.com>
2021-10-06 12:29:48 +0200

    add shopping card window
    Add an shopping card window when an Item is added.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
 console.log("Conected JS file")
+let shoppingCartStorage = window.localStorage.getItem('itemsInShoppingCount');
+
+let shoppingCart = document.querySelector("#addToCart");
+let shoppingCartCounter = document.querySelector(".shppingCardCounter");
+
+const addToCartButton = document.querySelector("#addToCart");
+const cartContent = document.querySelector(".cartContent");
+const closeButton = document.querySelector("#closeWindow");
+
+
+/* -------------------------------------------------------------------------- */
+/*                       Displays count on shopping card                      */
+/* -------------------------------------------------------------------------- */
+displayShoppingCartCount();
+function displayShoppingCartCount() {
+
+    if (shoppingCartStorage === null) {
+        shoppingCartStorage = 0;
+    } else {
+        shoppingCartCounter.innerHTML = `${shoppingCartStorage}`;
+    }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                    Adds number to shopping card counter                    */
+/* -------------------------------------------------------------------------- */
+function addToCart() {
+    if(shoppingCartStorage === null){
+        shoppingCartStorage = 0;
+    } else {
+        shoppingCartStorage++;
+        console.log(shoppingCartStorage);
+    }
+
+    window.localStorage.setItem('itemsInShoppingCount', shoppingCartStorage);
+    shoppingCartCounter.innerHTML = `${shoppingCartStorage}`;
+}
+
+shoppingCart.addEventListener("click", addToCart);
+
+function showCart(){
+    console.log("your shopping cart");
+    cartContent.classList.toggle("show");
+}
+addToCartButton.addEventListener("click", showCart);
+
+function closeCart(){
+    console.log("close")
+    cartContent.classList.remove("show");
+}
+closeButton.addEventListener("click", closeCart);
