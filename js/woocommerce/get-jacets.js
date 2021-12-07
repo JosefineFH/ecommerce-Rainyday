b2757175ba9fe897d70954fa942c2af4baacf6ae
Josefine Holth <Josefine.Holth@hotmail.com>
2021-12-07 21:13:13 +0100

    update

--- js/woocommerce/get-jacets.js
+++ js/woocommerce/get-jacets.js
@@ -1,17 +1,12 @@
 const url = "https://josefineholth.one/wp-json/wc/store/products?per_page=100";
 
-const weRecommend = document.querySelector(".we_recommend")
-const womansJackets = document.querySelector(".ladies_jackets")
+const weRecommend = document.querySelector(".we_recommend");
+const womansJackets = document.querySelector(".ladies_jackets");
 const mensJackets = document.querySelector(".mens-jackets");
-const onSale = document.querySelector(".on_sale")
+const onSale = document.querySelector(".on_sale");
 
-const discoundIcon = document.querySelector(".discount-icon");
+const productCardSetId = document.querySelector(".product_card");
 
-const productCardSetId = document.querySelector(".product_card")
-
-
-
-// let onsale;
 let tagsSlug;
 fetch(url)
     .then(response => response.json())
@@ -19,67 +14,66 @@ fetch(url)
         let products = data
 
         for (let i = 0; i < products.length; i++) {
-            // onsale = products[i].on_sale
             let tags = products[i].tags
+
             for (let t = 0; t < tags.length; t++) {
                 tagsSlug = tags[t].slug
+
+                
+                
                 if (tagsSlug === "recommended") {
                     weRecommend.innerHTML += `
                     <article class="product_card">
                     <a href="/specific-jacket-page.html?id=${products[i].id}">
-                    <div class="discount-icon">
-                    
-                    </div>
                     <img src="${products[i].images[0].src}" alt="${products[i].images[0].alt}">
                     <h3> ${products[i].name}</h3>
                     <p class="prise-card">${products[i].prices.currency_symbol} ${products[i].prices.price}</p>
                     </a>
-                    </article>`
-                }
+                    </article>`;
 
-                if (tagsSlug === "recommended-woman") {
+                } else if (tagsSlug === "recommended-woman") {
                     womansJackets.innerHTML += `
                     <article class="product_card">
                     <a href="/specific-jacket-page.html?id=${products[i].id}">
-                    <div class="discount-icon">
-                    
-                    </div>
                     <img src="${products[i].images[0].src}" alt="${products[i].images[0].alt}">
                     <h3> ${products[i].name}</h3>
                     <p class="prise-card">${products[i].prices.currency_symbol} ${products[i].prices.price}</p>                    
                     </a>
-                    </article>`
-                }
-
-                if (tagsSlug === "recommended-men") {
-                    mensJackets.innerHTML += ` <article class="product_card">
+                    </article>`;
+                } else if (tagsSlug === "recommended-men") {
+                    mensJackets.innerHTML += ` 
+                    <article class="product_card">
                     <a href="/specific-jacket-page.html?id=${products[i].id}">                    
-                    <div class="discount-icon">
-                    
-                    </div>
                     <img src="${products[i].images[0].src}" alt="${products[i].images[0].alt}">
-                    ${products[i].on_sale}
                     <h3> ${products[i].name}</h3>
-                    <p class="prise-card">${products[i].prices.currency_symbol} ${products[i].prices.price}</p>                    </a>
-                    </article>`
+                    <p class="prise-card">${products[i].prices.currency_symbol} ${products[i].prices.price}</p>                    
+                    </a>
+                    </article>`;
                 }
 
             }
             if (products[i].on_sale === true) {
-                console.log(products[i].on_sale)
-
-                onSale.innerHTML += ` <article class="product_card">
+                onSale.innerHTML += ` 
+                <article class="product_card">
                 <a href="/specific-jacket-page.html?id=${products[i].id}">                
-                <div class="discount-icon">
-                
-                </div>
                 <img src="${products[i].images[0].src}" alt="${products[i].images[0].alt}">
                 <h3> ${products[i].name}</h3>
                 <p class="prise-card">${products[i].prices.currency_symbol} ${products[i].prices.price}</p>
                 </a>
-                </article>`
+                </article>`;
             }
 
+
+            // const dispalyOnSale = document.querySelectorAll(".discount-icon");
+            // dispalyOnSale.classList.add("trues");
+
+            // if (products[i].on_sale == true) {
+            //     console.log(products[i].on_sale);
+            //     console.log(products[i].name);
+            //     // console.log(dispalyOnSale);
+            //     // console.log(dispalyOnSale)
+            //     // dispalyOnSale.innerHTML += `<p> On Sale </p>`
+            // }
         }
     })
     .catch(error => {
